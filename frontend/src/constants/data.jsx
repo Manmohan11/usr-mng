63f8374a0c46e13d76402a1eefee9aa9dddc934d
Manmohan Dwivedi <76547210+Manmohan11@users.noreply.github.com>
2024-07-12 15:19:29 +0530

    Update data.jsx

--- frontend/src/constants/data.jsx
+++ frontend/src/constants/data.jsx
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 
-axios.defaults.withCredentials = true;
-
 export const fetchData = async () => {
   try {
+    axios.defaults.withCredentials = true;
     console.log('Attempting to fetch data from https://usr-mng-api.vercel.app/api/users');
     const response = await axios.get('https://usr-mng-api.vercel.app/api/users');
     console.log('Data fetched successfully:', response.data);
